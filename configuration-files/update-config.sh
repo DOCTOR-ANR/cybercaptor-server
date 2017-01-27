f71f7e62c5a1907232bc9b7068127378a653dcaf
Théo Combe <theocombe@thalesgroup.com>
2017-01-27 17:39:05 +0100

    Added rules for bad face replies

--- configuration-files/update-config.sh
+++ configuration-files/update-config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp config.properties config.properties.root
-sed -i -e 's/\/home\/T0160040\/cybercaptor-server/\/root\/.remediation/g' config.properties.root
+sed -i -e 's/\/home\/T0160040\/projects\/CyberCAPTOR\/cybercaptor-server/\/root\/.remediation/g' config.properties.root
